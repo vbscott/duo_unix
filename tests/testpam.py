2293f48d8441a35dc3ec9b482f9c66f9fccfa2a6
Briar Scott <bscott@bscott-14399.local>
2021-10-29 09:56:27 -0400

    Trying a newer thing

--- tests/testpam.py
+++ tests/testpam.py
@@ -61,6 +61,8 @@ def main():
     else:
         env['LD_PRELOAD'] = paths.build + '/.libs/libtestpam_preload.so'
         
+    print("About to open the thing")
+    print(args)
     p = subprocess.Popen(args, env=env)
     p.wait()
     f.close()
